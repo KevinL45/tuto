00ebee935bbfd5c46426a61f5abd454e3baeaff1
Kevin Lokoka <68908952+KevinL45@users.noreply.github.com>
2022-02-17 12:34:06 +0100

    nodemon

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 //On installe les modules.
-//Pas en global car en moment du démarrage du serveur, il va pas retrouver les modules
-//Rapelle pour installer un module en global : npm install -g ...
 // npm install nodemon
 // npm install babel-register
 // npm install express
+
+//Pas en global car en moment du démarrage du serveur, il va pas retrouver les modules
+//Rapelle pour installer un module en global : npm install -g ...
+
 
 //Création d'un package.json
 // npm init 
@@ -17,7 +19,8 @@ const express = require("express")
 //ça permet d'utiliser la méthoode POST
 const bodyParser = require("body-parser");
 
-
+const {success, error} = require('./function');
+const { memberExpression } = require("babel-types");
 
 //Création d'une instance express
 const app = express()
@@ -34,15 +37,88 @@ app.use((req,res,next)=>{
     next()
 })
 
+
+const member = [{
+    id :1,
+    name : 'Kevin',
+
+    id:2,
+    name:'John',
+
+    id:3,
+    name:'Jack'
+}]
+
+//functions
+
+function getIndex(id){
+    for (let i = 0; i < member.length; i++){
+        if(member[i].id == id){
+            return i;
+        }
+    }
+}
+
+function createId(){
+    //On retourne l'id du dernier membre
+    return member[member.length - 1].id + 1
+}
+
+//get
+
 app.get('/api', function(req,res){
-    res.send('Bienvenue')
-    
+    res.send('Bienvenue') 
 })
 
-app.get('api/book/:id',(req,res)=>{
-        //Il permet de récupérer les paramètre de la route
-        res.send(req.param)
+app.get('/api/member/:id',(req,res)=>{
+    //Il permet de récupérer l'index de member
+    let index = getIndex(req.params.id)
+
+    //Si la variable index est un string
+    if (typeof(index) == 'string'){
+
+        //On retourne une erreur
+        res.json(error(index))
+    }else{
+        //Sinon on retourne le member
+        res.json(success(member[index]))
+    }
 })
+
+app.get('/api/members',(req,res)=>{
+    
+    //Si la requête contient un paramètre max
+    if(req.query.max != undefined && req.query.max > 0){
+
+        //On renvoie les membre de 0 à max
+        res.json(sucess(member.slice(0,req.query.max)))
+
+    //Si la requête contient un paramètre vide   
+    }else if(req.query.max != undefined){
+
+        //On renvoi un paramètre vide
+        res.json(error("Mauvaise valeur"))
+
+    //Sinon on retourne tout les membres
+    }else{
+        res.json(success(member))
+    }
+})
+
+//post
+app.post('/api/members',(req,res)=>{
+    if(req.body.name != undefined){
+        member.push({
+            id: createId(),
+            name : req.body.name
+        })
+        res.json(success(member))
+    }else{
+        res.json(error("Missing name"))
+    }
+})
+
+
 
 app.listen(8000,() => {
     console.log("Commencer")
